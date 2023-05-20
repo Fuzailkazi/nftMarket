197e1d91bcf806d1843af0a80490c5bbd226dd38
Fuzailkazi <mohdfuzailkazi14@gmail.com>
2023-05-20 22:31:20 +0530

    contract ready & unit testing done

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,12 @@
 require("@nomicfoundation/hardhat-toolbox");
-
+require("dotenv").config();
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.18",
+  networks: {
+    sepolia: {
+      url: process.env.INFURA_URL,
+      accounts: [process.env.WALLET_PRIVATE_KEY],
+    },
+  },
 };
